bf06e69f8d24ecfb964fb4e5386774e28ae3696e
Hasan Imran <hasanimran_96@yahoo.com>
2019-12-05 16:07:29 +0100

    attempt #1 at BCP

--- solver/example.py
+++ solver/example.py
@@ -22,18 +22,29 @@ print(type(clauses))
 trail = []
 
 
-# def DPLL(CNF_formula):
-#     trail.clear
-#     if(BCP() == False):
-#         return 'UNSAT'
-#     while(True):
-#         if(decide() == False):
-#             return 'SAT'
-#         while(BCP() == False):
-#             if(backtrack() == False):
-#                 return
+def DPLL(CNF_formula):
+    trail.clear
+    if(BCP() == False):
+        return 'UNSAT'
+    while(True):
+        if(decide() == False):
+            return 'SAT'
+        while(BCP() == False):
+            if(backtrack() == False):
+                return
 
-# def check_unit(clauses):
+
+def check_unit(clauses):
+    for i in range(len(clauses)):
+        if(len(clauses[i]) == 1):
+            v = 1
+            trail.append(clauses[i], v)
+        for literal in range(len(clauses[i])):
+            while(literal.value != 1):
+                continue
+            if(literal.value == 0):
+                return False
+    return True
 
 # def BCP():
 #     # while(there is a unit clause implying that a variable x must be set to a value v):
@@ -42,19 +53,24 @@ trail = []
 #         # 	return False
 #     return True
 
-# def decide():
-#     # if(all variables are assigned):
-#     #     return False
-#     # choose unassigned variable x
-#     # v = 0 or v = 1
-#     # trail.push(x, v, false)
-#     return True
 
-# def backtrack():
-#     while(True):
-#         if(len(trail) == 0):
-#             return False
-#         x, v, b = trail.pop
-#         if(b == False):
-#             trail.append(x, v, True)
-#             return True
+def decide():
+    for clause in range(len(clauses)):
+        for literal in clause:
+            if(literal.value != None):
+                return False
+            # assigning false as default
+            v = 0
+            trail.append(literal, v)
+            return True
+
+
+def backtrack():
+    while(True):
+        if(len(trail) == 0):
+            return False
+        x, v = trail.pop()
+        if(v == 0):
+            v = 1
+            trail.append(x, v)
+            return True
